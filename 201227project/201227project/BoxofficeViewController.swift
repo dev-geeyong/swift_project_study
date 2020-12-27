3bad7d5698947c8f93be67000cb8538e8cffbd8c
dev-geeyong <dev.geeyong@gmail.com>
2020-12-27 17:58:52 +0900

    서버통신API

--- 201227project/201227project/BoxofficeViewController.swift
+++ 201227project/201227project/BoxofficeViewController.swift
@@ -12,23 +12,58 @@ import JGProgressHUD
 
 class BoxofficeViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
     @IBOutlet var nmLabel1: UILabel!
     
     @IBOutlet var nmLabel2: UILabel!
     
-   
+    @IBOutlet var textField: UITextField!
+    
     @IBOutlet var nmLabel3: UILabel!
+    
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    
+    
+    @IBAction func clickButton(_ sender: Any) {
+        //숫자, 8자리, 입력한 내용이 맞는지 확인하는..
+        
+        var test = textField.text
+        callRequest(date: test!)
+        
+        
+    }
     let hud = JGProgressHUD()
     
     override func viewDidLoad() {
         
        
         super.viewDidLoad()
+        callRequest(date: "20111111")
+
+    }
+    //labelOutletNameFromStoryBoard, labelTextColor 보충설명 가능 위에서 사용되는 외부매개변수
+    // _ 와일드카드 식별자
+    //label, color 안에서만 사용.. 내부매개변수
+    
+    func moviewTitleDesignSetting(_ label: UILabel, labelTextColor color: UIColor){
         
+        label.font = .boldSystemFont(ofSize: 28)
+        label.textColor = color
+        label.layer.cornerRadius = 10
+        label.layer.borderWidth = 1
+        label.layer.borderColor = UIColor.darkGray.cgColor
+        label.textAlignment = .center
+    }
+    func callRequest(date : String){
         self.hud.textLabel.text = "Loading"
         self.hud.show(in: self.view)
+            
+        moviewTitleDesignSetting(self.resultLabel, labelTextColor: .red)
+        moviewTitleDesignSetting(self.resultLabel2, labelTextColor: .blue)
+        moviewTitleDesignSetting(self.resultLabel3, labelTextColor: .brown)
         
-        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=6435e44991ae7456ca40818fa990c14e&targetDt=20201220"
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=6435e44991ae7456ca40818fa990c14e&targetDt=\(date)"
         
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
@@ -41,9 +76,11 @@ class BoxofficeViewController: UIViewController {
                 
                 let movie2 = json["boxOfficeResult"]["dailyBoxOfficeList"][1]["movieNm"].stringValue
                 let movie3 = json["boxOfficeResult"]["dailyBoxOfficeList"][2]["movieNm"].stringValue
+                
                 self.nmLabel1.text = movie1
                 self.nmLabel2.text = movie2
                 self.nmLabel3.text = movie3
+                
                 print(movie1)
                 
                 self.hud.dismiss()
@@ -51,9 +88,7 @@ class BoxofficeViewController: UIViewController {
                 print(error)
             }
         }
-
     }
-    
 
     }
 
