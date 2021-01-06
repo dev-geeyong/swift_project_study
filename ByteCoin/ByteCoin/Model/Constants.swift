5fc120539cebdcd07f7b0c350f2badecda2b1a1d
dev-geeyong <dev.geeyong@gmail.com>
2021-01-06 23:47:59 +0900

    - cocoapods install Firebase - Firebase Authentication , Firestore - tableViews - custom UI .xib

---
+++ ByteCoin/ByteCoin/Model/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by dev.geeyong on 2021/01/06.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash-Chat/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by dev.geeyong on 2021/01/06.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet var messageBackgroundView: UIView!
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var ImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBackgroundView.layer.cornerRadius = messageBackgroundView.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
